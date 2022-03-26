1e14e90f5ac57753faae7480516a9d65d6d71a9b
noco <k-nitrate.1002@ezweb.ne.jp>
2022-03-26 17:08:32 +0900

    Update lazy.hs

--- lazy.hs
+++ lazy.hs
@@ -5,23 +5,19 @@ import System.IO(hSetBinaryMode, hSetBuffering, stdin, stdout, BufferMode (NoBuf
 data Expr = S | K | I | Iota | A Expr Expr
     | CInt Int | CSucc | CList [Expr]
 
-eval :: Expr -> Expr
-eval (A x y) = apply (eval x) $ eval y
-    where
-        apply :: Expr -> Expr -> Expr
-        apply (A (A S x) y) z = apply (apply x z) $ apply y z
-        apply (A K x) y = x
-        apply I x = x
-        apply Iota x = apply (apply x S) K
-        apply (A (CInt i) x) y = iterate (apply x) y !! i
-        apply CSucc (CInt i) = CInt $ i + 1
-        apply (CList (x : t)) y = apply (apply y x) $ CList t
-        apply x y = A x y
-eval x = x
+app :: Expr -> Expr -> Expr
+app (A (A S x) y) z = app (app x z) $ app y z
+app (A K x) y = x
+app I x = x
+app Iota x = app (app x S) K
+app (A (CInt i) x) y = iterate (app x) y !! i
+app CSucc (CInt i) = CInt $ i + 1
+app (CList (x : t)) y = app (app y x) $ CList t
+app x y = A x y
 
 parse :: [Char] -> Expr
 parse [] = I
-parse x = foldl1 A $ map fromIota $ parse' $ tail $ scanl f (' ', 0) $ filter (`elem` "()*01IKS`iks") x
+parse x = foldl1 app $ map fromIota $ parse' $ tail $ scanl f (' ', 0) $ filter (`elem` "()*01IKS`iks") x
     where
         fromIota :: Expr -> Expr
         fromIota Iota = I
@@ -34,13 +30,13 @@ parse x = foldl1 A $ map fromIota $ parse' $ tail $ scanl f (' ', 0) $ filter (`
             | x `elem` "Kk" = K : parse' t
             | x `elem` "Ss" = S : parse' t
             | x `elem` "01" = (\ (x, y) -> foldl g I x : parse' y) $ span ((`elem` "01") . fst) $ (x, i) : t
-            | x == '`' = (\ (x : y : t) -> A (fromIota x) (fromIota y) : t) $ parse' t
-            | x == '*' = (\ (x : y : t) -> A x y : t) $ parse' t
-            | x == '(' = (\ (x, y) -> foldl1 A (fromIota <$> parse' x) : parse' (tail y)) $ span ((>= i) . snd) t
+            | x == '`' = (\ (x : y : t) -> app (fromIota x) (fromIota y) : t) $ parse' t
+            | x == '*' = (\ (x : y : t) -> app x y : t) $ parse' t
+            | x == '(' = (\ (x, y) -> foldl1 app (fromIota <$> parse' x) : parse' (tail y)) $ span ((>= i) . snd) t
             where
                 g :: Expr -> (Char, Int) -> Expr
-                g x ('0', _) = A (A x S) K
-                g x _ = A S $ A K x
+                g x ('0', _) = app (app x S) K
+                g x _ = app S $ app K x
         parse' _ = []
 
         f :: (Char, Int) -> Char -> (Char, Int)
@@ -54,11 +50,11 @@ unchurch = map toEnum . takeWhile (< 256) . map uncInt . uncList
     where
         uncInt :: Expr -> Int
         uncInt (CInt i) = i
-        uncInt x = uncInt $ eval $ A (A x CSucc) $ CInt 0
+        uncInt x = uncInt $ app (app x CSucc) $ CInt 0
 
         uncList :: Expr -> [Expr]
         uncList (CList l) = l
-        uncList x = eval (A x K) : uncList (eval $ A x $ A S K)
+        uncList x = app x K : uncList (app x $ app S K)
 
 main :: IO ()
 main = do
@@ -69,7 +65,7 @@ main = do
     input <- getContents
     hSetBinaryMode stdout b
     hSetBuffering stdout NoBuffering
-    putStr $ unchurch $ A (foldl1 A $ parse <$> reverse codes) $ church input
+    putStr $ unchurch $ app (foldl1 app $ parse <$> reverse codes) $ church input
     where
         f :: [[Char]] -> [IO [Char]]
         f ("-e" : x : t) = pure x : f t
